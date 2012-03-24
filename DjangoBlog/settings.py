42d7db728906068ac34cbbc83b40d264a59a1d40
Archer F <DarkTemplar.f@gmail.com>
2012-03-25 03:34:02 +0800

    gunicorn

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -121,7 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    "DjangoBlog.blog"
+    "DjangoBlog.blog",
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
