87305f6a3226344b91e5abfcd4036cb3509b9470
Feng Liyuan <DarkTemplar.f@gmail.com>
2012-03-29 04:09:26 +0800

    try static files problem 10

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -61,12 +61,12 @@ STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'sitestatic').replace('\\'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://sunrunaway.herokuapp.com/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = 'http://sunrunaway.herokuapp.com/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
