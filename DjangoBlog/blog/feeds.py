7aedc260a073128eb69b8eead4a76d9834576cf8
Feng Liyuan <DarkTemplar.f@gmail.com>
2012-03-31 11:40:57 +0800

    try rss feeds

--- DjangoBlog/blog/feeds.py
+++ DjangoBlog/blog/feeds.py
@@ -1,16 +1,26 @@
 from django.contrib.syndication.views import Feed
-from blog.models import Blog
+from blog.models import Blog, Category
+
 
 class LastEntriesFeeds(Feed):
     title = "Sun Run Away"
-    link = "/blog/view/"
+    link = "/"
     description = "Get rss feeds from my blog"
 
     def items(self):
-        return Blog.objects.all()[:5]
+        return Blog.objects.all()[:20]
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
-        return item.body[:20]
+        return item.body
+
+    def item_categories(self, item):
+        return tuple(item.category.values_list('title', flat=True))
+
+    def item_pubdate(self, item):
+        return item.posted
+
+    def categories(self):
+        return Category.objects.all()
