8c296916c8dd868a10f0128d8671577b4eb1d427
Feng Liyuan <DarkTemplar.f@gmail.com>
2012-04-02 13:36:08 +0800

    display META

--- DjangoBlog/views.py
+++ DjangoBlog/views.py
@@ -6,7 +6,8 @@ current_site.domain
 
 
 def displayMeta(request):
-    html = ['<tr><td>current_site.domain</td><td>%s</td></tr>' % (current_site.domain)]
-    for k, v in request.META.items():
-        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
-    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+    # html = ['<tr><td>current_site.domain</td><td>%s</td></tr>' % (current_site.domain)]
+    # for k, v in request.META.items():
+    #     html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    # return HttpResponse('<table>%s</table>' % '\n'.join(html))
+    return None
