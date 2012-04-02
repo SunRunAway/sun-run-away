dce308c3f5341f41d2ab3f063d69b730b5178193
Feng Liyuan <DarkTemplar.f@gmail.com>
2012-04-02 13:27:28 +0800

    current_site.domain

--- DjangoBlog/views.py
+++ DjangoBlog/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+from django.contrib.sites.models import Site
+
+current_site = Site.objects.get_current()
+current_site.domain
 
 
 def displayMeta(request):
-    html = []
+    html = ['<tr><td>current_site.domain</td><td>%s</td></tr>' % (current_site.domain)]
     for k, v in request.META.items():
         html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
     return HttpResponse('<table>%s</table>' % '\n'.join(html))
